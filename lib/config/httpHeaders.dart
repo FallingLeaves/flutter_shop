76bf87c1f8270858715e1e096999b6f5a2641fa1
FallingLeaves <1959037844@qq.com>
2019-11-02 09:57:59 +0800

    flutter_shop首页即首页数据

---
+++ lib/config/httpHeaders.dart
@@ -0,0 +1,18 @@
+const httpHeaders = {
+  'Accept': 'application/json, text/plain, */*',
+  'Accept-Encoding': 'gzip, deflate, br',
+  'Accept-Language': 'zh-CN,zh;q=0.9',
+  'Cache-Control': 'no-cache',
+  'Connection': 'keep-alive',
+  'Content-Type': 'application/json',
+  'Cookie':
+      '_ga=GA1.2.1031855286.1572439921; _gid=GA1.2.913058289.1572439921; _gat=1; GRID=8c9213b-b30c883-bdd759a-baa91d1; MEIQIA_TRACK_ID=1SvN4nJ7Q5CBCmqExAdWA5eFbuj; MEIQIA_VISIT_ID=1SvN4nMKKdmnOxxK3zK6WcybdYA; SERVERID=3431a294a18c59fc8f5805662e2bd51e|1572439926|1572439899; Hm_lvt_022f847c4e3acd44d4a2481d9187f1e6=1572439921,1572439929; Hm_lpvt_022f847c4e3acd44d4a2481d9187f1e6=1572439929',
+  'Host': 'time.geekbang.org',
+  'Origin': 'https://time.geekbang.org',
+  'Pragma': 'no-cache',
+  'Referer': 'https://time.geekbang.org/',
+  'Sec-Fetch-Mode': 'cors',
+  'Sec-Fetch-Site': 'same-origin',
+  'User-Agent':
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36'
+};
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import '../service/service_method.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'dart:convert';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          '百姓生活+',
+        ),
+      ),
+      body: FutureBuilder(
+        future: getHomePageContent(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            var data = json.decode(snapshot.data.toString());
+            List<Map> swiper = (data['data']['slides'] as List).cast();
+
+            return Column(
+              children: <Widget>[
+                SwiperDiy(
+                  swiperDateList: swiper,
+                )
+              ],
+            );
+          } else {
+            return Center(
+              child: Text(
+                '加载中...',
+              ),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+// 首页轮播组件
+class SwiperDiy extends StatelessWidget {
+  final List swiperDateList;
+
+  SwiperDiy({Key key, this.swiperDateList}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    ScreenUtil.instance = ScreenUtil(
+      width: 750,
+      height: 1334,
+    )..init(context);
+
+    print('设备像素密度: ${ScreenUtil.pixelRatio}');
+    print('设备高: ${ScreenUtil.screenHeight}');
+    print('设备宽: ${ScreenUtil.screenWidth}');
+
+    return Container(
+      height: ScreenUtil().setHeight(333),
+      width: ScreenUtil().setWidth(750),
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network(
+            '${swiperDateList[index]['image']}',
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: swiperDateList.length,
+        pagination: SwiperPagination(),
+        autoplay: true,
+      ),
+    );
+  }
+}
