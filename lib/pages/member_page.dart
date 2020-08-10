af56ca4b2b1a22eab3c94e8ab67c70aaaaf7453c
FallingLeaves <1959037844@qq.com>
2020-08-10 21:27:25 +0800

    用户中心UI

--- lib/pages/member_page.dart
+++ lib/pages/member_page.dart
@@ -13,6 +13,7 @@ class MemberPage extends StatelessWidget {
           _topHeader(),
           _orderTitle(),
           _orderType(),
+          _actionList(),
         ],
       ),
     );
@@ -128,6 +129,40 @@ class MemberPage extends StatelessWidget {
               ],
             ),
           ),
+        ],
+      ),
+    );
+  }
+
+  Widget _myListTile(String title) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          bottom: BorderSide(
+            width: 1,
+            color: Colors.black12,
+          ),
+        ),
+      ),
+      child: ListTile(
+        leading: Icon(Icons.blur_circular),
+        title: Text(title),
+        trailing: Icon(Icons.arrow_right),
+      ),
+    );
+  }
+
+  Widget _actionList() {
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      child: Column(
+        children: <Widget>[
+          _myListTile('领取优惠券'),
+          _myListTile('已领取优惠券'),
+          _myListTile('地址管理'),
+          _myListTile('客服电话'),
+          _myListTile('关于我们'),
         ],
       ),
     );
